db1da856c835e55f71158855cd4b0aeefe2f55ac
jeocarlolubao <jeocarlolubao@gmail.com>
2023-12-13 21:38:10 +0800

    faq section

---
+++ src/routes/faq/+page.svelte
@@ -0,0 +1,101 @@
+<!-- 
+Creating an FAQ section for your uptime website tracker is a great idea to address common queries users may have. Here are some potential questions to include:
+
+1. **What is the purpose of this website?**
+   - Provide a brief overview of the purpose and functionality of your uptime tracker.
+
+2. **How does the website tracker work?**
+   - Explain the process of how your tracker monitors the uptime of student portals and school websites.
+
+3. **Which schools in Davao City are included in the tracker?**
+   - List the schools covered by your tracker to give users an idea of the scope of your monitoring.
+
+6. **Can I rely on the information provided by the tracker?**
+   - Clarify the accuracy and reliability of the uptime information, and explain any limitations users should be aware of.
+
+7. **Is the website tracker accessible on mobile devices?**
+   - Confirm whether the tracker is optimized for mobile usage and if there's a dedicated mobile version or app.
+
+8. **How can I report issues or inaccuracies with the uptime data?**
+   - Provide contact information or a submission form for users to report any problems or inaccuracies they may encounter.
+
+9. **Is the service free to use?**
+   - Clearly state whether there are any costs associated with using the website tracker and if there are any premium features.
+
+10. **Can I get notifications for downtime events?**
+    - Explain if users have the option to receive notifications, such as email alerts, when a school portal experiences downtime.
+
+11. **How can I support or contribute to the uptime tracker?**
+    - If applicable, provide information on how users can contribute data, provide feedback, or support the development and maintenance of the tracker.
+
+12. **Are there any privacy concerns with using the uptime tracker?**
+    - Address any privacy-related concerns users may have and explain how you handle and protect user data.
+
+Tailor these questions to fit the specifics of your uptime tracker and the needs of your users.
+
+ -->
+
+<body class="flex flex-col w-full h-full mb-auto bg-slate-100">
+	<h1
+		class="flex flex-row border font-ibm-plex-sans font-normal mx-[350px] my-2 w-fit text-8xl mt-20"
+	>
+		FAQ
+	</h1>
+	<h4 class="flex flex-row border font-ibm-plex-sans font-normal mx-[350px] my-2 w-fit text-xl">
+		<strong>F</strong>requently &nbsp; <strong>A</strong>sked &nbsp; <strong>Q</strong>uestions
+	</h4>
+	<hr class="mx-[300px] border-black my-4 mt-2" />
+	<h1 class="mx-[350px] my-4">About Project DESTINY</h1>
+	<h1 class="mx-[350px] my-4">What is Project DESTINY?</h1>
+	<p class="mx-[350px] my-4">
+		Project DESTINY serves as a centralized directory for checking the uptime of websites and
+		student portals of various educational institutions, offering a convenient and single point of
+		access for users across Davao City. Its main goal is to provide a centralized directory for
+		educators and students to check if their school's website is up.
+	</p>
+	<h1 class="mx-[350px] my-4">How does the website tracker work?</h1>
+	<p class="mx-[350px] my-4">
+		The website tracker uses a CORS proxy to verify the functionality of school websites. This proxy
+		acts like a messenger, checking if the websites are open. If the response is positive, it means
+		the website is working. However, if there's a problem , the website is experiencing issues.
+		Essentially, the CORS proxy functions as an efficient assistant, promptly notifying users about
+		the accessibility of their school's online resources.
+	</p>
+	<h1 class="mx-[350px] my-4">Which schools in Davao City are included in the tracker?</h1>
+	<p class="mx-[350px] my-4">
+		As of *insert date here* the following schools included in the tracker are the following
+	</p>
+	<ol class="mx-[350px] my-4 list-disc">
+		<li>Ateneo de Davao University (ADDU)</li>
+		<li>Holy Cross of Davao College (HCDC)</li>
+		<li>Lyceum of the Philippines University - Davao (LPU - Davao)</li>
+		<li>Mapua Malayan Colleges Mindanao (MMCM)</li>
+		<li>University of the Immaculate Conception (UIC)</li>
+		<li>University of Mindanao (UM)</li>
+		<li>University of Southeastern Philippines (USEP)</li>
+	</ol>
+	<h1 class="mx-[350px] my-4">Can I rely on the information provided by the tracker?</h1>
+	<p class="mx-[350px] my-4">
+		Absolutely. The website tracker not only monitors the uptime of various student portals and
+		school websites but also provides direct links to each website. This feature allows users to
+		double-check the information by visiting the websites themselves. The transparency in offering
+		direct links enhances the reliability of the tracker, enabling users to independently verify the
+		status of their school's online platforms and ensuring accurate and up-to-date information.
+	</p>
+	<h1 class="mx-[350px] my-4">Is the website tracker accessible on mobile devices?</h1>
+	<p class="mx-[350px] my-4">
+		Yes, the website tracker is accessible on mobile devices. However, ongoing improvements are in
+		progress to enhance user experience further. The development efforts include making the website
+		a Progressive Web App (PWA) and optimizing it to be more mobile-friendly. These enhancements aim
+		to provide users with a seamless and responsive experience while accessing the tracker on
+		various mobile devices, ensuring ease of use and accessibility on the go.
+	</p>
+	<h1 class="mx-[350px] my-4">How can I report issues or inaccuracies with the uptime data?</h1>
+	<p class="mx-[350px] my-4">
+		You may fill up this Google form or you may submit a GitHub issue <a
+			href="https://github.com/jeoooo/PROJECTDESTINY/issues"
+			target="_blank"
+			rel="noopener noreferrer">here</a
+		>
+	</p>
+</body>
